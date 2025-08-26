a0447253c93804758982980efd94a99d40aa9d56
Jitin Kukreja <jitinkukreja00@gmail.com>
2025-08-26 10:11:34 +0530

    mvp ready

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,3 +1,7 @@
+/* eslint-disable @typescript-eslint/no-explicit-any*/
+/* eslint-disable @typescript-eslint/no-unused-vars*/
+
+
 import NextAuth, { NextAuthOptions, User } from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
 import { EntryModel } from "../../../../../backend/models/Schema";
